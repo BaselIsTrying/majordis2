4d5d31678b0228177a390daa1baa9e4ac99d9c26
BaselIsTrying <baseldiscord1@gmail.com>
2024-12-29 23:21:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  readyStatus: false,
+  checkUpdate: false
+});
+//environment
+require('dotenv').config()
+
+function formatTime() { 
+  const date = new Date();
+  const options = {
+    timeZone: 'America/New_York', 
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+const express = require("express")
+const app = express();
+var listener = app.listen(process.env.PORT || 2000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+app.listen(() => console.log("I'm Ready To Work..! 24H"));
+app.get('/', (req, res) => {
+  res.send(`
+  <body>
+  <center><h1>Bot 24H ON!</h1></center
+  </body>`)
+});
+client.on('ready', async () => {
+  console.clear();
+  console.log(`${client.user.tag} - rich presence started!`);
+
+  const r = new Discord.RichPresence()
+    .setApplicationId('1265825059692609587')
+    .setType('PLAYING')
+    .setURL('https://www.twitch.tv/apparentlyjack_rl') 
+    .setState('NOW GET THE FUCK OUT')
+    .setName('CALM SHARK')
+    .setDetails(`Welcome to Mr.Sharks Profile`)
+    .setStartTimestamp(Date.now())
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/918918699203837982/1321501402643562618/itsoma98_14031001_172038562.jpg?ex=6770c356&is=676f71d6&hm=de3a303f953373ac46648b569a8c65cb4e38a7184a83ac4c9a6712288f797a7b&=&format=webp&width=692&height=521') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('I AM BASEL :)') 
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1193602666518036541/1321507550021156874/Picsart_24-12-25_18-59-03-230.jpg?ex=6770c910&is=676f7790&hm=49e0afbe61950231a563a9c7ef35e67dd0a32ec6eecb3d16355d8f95d1e5dada&=&format=webp&width=701&height=701') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('UNMESSABLE') 
+    .addButton('Google', 'https://google.com');
+
+  client.user.setActivity(r);
+  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+
+});
+
+const mySecret = process.env['TOKEN'];
+client.login(mySecret);
